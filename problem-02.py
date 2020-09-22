e866d007670ba548cc0fbf795b796e4a83839035
Adam Catto <adamcatto@Adams-MBP.lan1>
2020-09-22 14:44:42 -0400

    problem 02 matrix operations

--- problem-02.py
+++ problem-02.py
@@ -14,5 +14,14 @@ def compute_correlation_matrix_loop(x):
 
 def compute_correlation_matrix_ops(x):
     num_vectors = x.shape[0]
-    feature_dim = x.shape[1]
-    
+    y = x - np.matmul(np.ones((num_vectors, num_vectors)), x)/num_vectors
+    vcv = (np.matmul(np.transpose(y), y)/num_vectors)
+    d = np.linalg.inv(np.sqrt(np.diag(np.diag(vcv))))
+    cov_matrix = np.matmul(np.matmul(d, vcv), d)
+    return cov_matrix
+
+
+r = np.random.rand(10, 9)
+
+print(compute_correlation_matrix_loop(r))
+print(compute_correlation_matrix_ops(r))
