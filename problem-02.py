a1e9e790fa9d7c6dde4a1e9775a443cd39df00f4
Adam Catto <adamcatto@Adams-MBP.lan1>
2020-09-22 13:07:52 -0400

    problem 02 loop

--- problem-02.py
+++ problem-02.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-def compute_correlation_matrix_loop():
-    pass
+from utils import covar, sd
 
 
-def compute_correlation_matrix_ops():
-    pass
+def compute_correlation_matrix_loop(x):
+    feature_dim = x.shape[1]
+    z = np.zeros((feature_dim, feature_dim))
+    for i in range(feature_dim):
+        for j in range(feature_dim):
+            z[i][j] = covar(x, (i, j)) / (sd(x, i) * sd(x, j))
+    return z
+
+
+def compute_correlation_matrix_ops(x):
+    num_vectors = x.shape[0]
+    feature_dim = x.shape[1]
+    
